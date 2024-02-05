b20d147347d6fea9b4a8cc1c389e58ddd03a35a9
surya pratap singh tomar <87597833+suryapratapsingh5674@users.noreply.github.com>
2024-02-05 21:32:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('#main'),
+    smooth: true
+});
+
 function circleMouseFollower(xscale, yscale) { 
     window.addEventListener("mousemove", function (dets) {
       document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
